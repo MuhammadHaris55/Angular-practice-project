f9e40a3b5c9cfdaab43c1af43e3bf06fe8d06cb1
MuhammadHaris55 <hkhn567@gmail.com>
2024-02-26 11:34:02 +0500

    Interpolation, Angular CLI, Component(normal, inline-template, inline-style), Module and Functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,18 @@ import { UserAuthModule } from './user-auth/user-auth.module';
 })
 export class AppComponent {
   title = 'blog';
+
+  functionCall() {
+    alert('function call');
+  }
+  // alertName(name) {
+  //   alert('My name is ' + name)
+  // }
+  // The above function didn't give error in Angular 9, 10, 11 
+    // but for Angular 12 we must have to define the dataType of our variables
+    // because in Angular 12 strict mode is true in tsconfig.json in Angular 12
+    // so the compiler strictly folow the rules, we can make it false to avoid these strictness of rules
+  alertName(name:string) {
+    alert('My name is ' + name)
+  }
 }
